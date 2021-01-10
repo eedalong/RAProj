5a14ee9377f1b0a6268f2083a960b308f3d02ef5
Dalong <39682259+eedalong@users.noreply.github.com>
2021-01-10 20:09:04 +0800

    add res convert code

---
+++ convert.py
@@ -0,0 +1,23 @@
+import csv
+input_file = open("res_file")
+res_file = open("convert_res.csv", "w", newline='')
+csv_writer = csv.writer(res_file)
+while(True):
+    try:
+        line1 = next(input_file)
+        line2 = next(input_file)
+    except StopIteration:
+        break
+    data = line1.split("|")
+    data = list(filter(lambda x: x != "," and x != '\n', data))[1:]
+    tmp = data[8].split(",")
+    data[8] = tmp[0]
+    if len(tmp) > 1:
+        data.insert(9, tmp[1])
+    data2 = line2[1:].split("|")[1:]
+    data2[-1] = data2[-1][-1]
+    data.extend(data2)
+    csv_writer.writerow(data)
+
+res_file.close()
+
